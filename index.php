64e2278e65f4c1af8ad6db0d7070a05582b10e52
casconalio <compto51@msu.edu>
2016-02-16 15:20:48 -0500

    Inital commit. Added index, style, and images.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <title>CSE 477 Project 1</title>
+    <style type="text/css"></style>
+</head>
+<body>
+<p>hey cash</p>
+</body>
+</html>
+

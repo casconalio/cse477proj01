aa34ddc43879a5a2e771c850f1ad6d98207c0c7a
casconalio <compto51@msu.edu>
2016-02-16 16:34:12 -0500

    renamed

--- tests/tests/SteampunkedTest.php
+++ tests/tests/SteampunkedTest.php
@@ -4,9 +4,9 @@ require __DIR__ . "/../../vendor/autoload.php";
 /** @file
  * @brief Empty unit testing template
  * @cond 
- * @brief Unit tests for the Steampunked Model class
+ * @brief Unit tests for the Steampunked class
  */
-class ModelTest extends \PHPUnit_Framework_TestCase
+class SteampunkedTest extends \PHPUnit_Framework_TestCase
 {
 	public function test1() {
 		//$this->assertEquals($expected, $actual);

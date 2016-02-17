b4a32c0dd21d831731a20126dc528e0c18cf8d01
Hanlin Ye <y1196293@gmail.com>
2016-02-17 01:36:26 -0500

    commit

--- lib/Steampunked/Steampunked.php
+++ lib/Steampunked/Steampunked.php
@@ -11,5 +11,36 @@ namespace Steampunked;
 
 class Steampunked
 {
-    ////test
+    private $size;
+
+    private $pipe=array();
+
+
+    public function __construct($seed = null) {
+        if($seed === null) {
+            $seed = time();
+        }
+        srand($seed);
+}
+    public function pipes(){
+        $this->pipe[] = new Tile("cap-e.png", array("N" => false, "W" => false, "S" => false, "E" => true));
+        $this->pipe[] = new Tile("cap-n.png", array("N" => true, "W" => false, "S" => false, "E" => false));
+        $this->pipe[] = new Tile("cap-s.png", array("N" => false, "W" => false, "S" => true, "E" => false));
+        $this->pipe[] = new Tile("cap-w.png", array("N" => false, "W" => true, "S" => false, "E" => false));
+        $this->pipe[] = new Tile("ninety-es.png", array("N" => false, "W" => false, "S" => true, "E" => true));
+        $this->pipe[] = new Tile("ninety-ne.png", array("N" => true, "W" => false, "S" => false, "E" => true));
+        $this->pipe[] = new Tile("ninety-sw.png", array("N" => false, "W" => true, "S" => true, "E" => false));
+        $this->pipe[] = new Tile("ninety-wn.png", array("N" => true, "W" => true, "S" => false, "E" => false));
+        $this->pipe[] = new Tile("straight-h.png", array("N" => false, "W" => true, "S" => false, "E" => true));
+        $this->pipe[] = new Tile("straight-v.png", array("N" => true, "W" => false, "S" => true, "E" => false));
+        $this->pipe[] = new Tile("tee-esw.png", array("N" => false, "W" => true, "S" => true, "E" => true));
+        $this->pipe[] = new Tile("tee-nes.png", array("N" => true, "W" => false, "S" => true, "E" => true));
+        $this->pipe[] = new Tile("tee-swn.png", array("N" => true, "W" => true, "S" => true, "E" => false));
+        $this->pipe[] = new Tile("tee-wne.png", array("N" => true, "W" => true, "S" => false, "E" => true));
+
+    }
+    public function randomPipes(){
+
+
+    }
 }
